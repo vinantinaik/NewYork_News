2c5f145d165559c7ef8a980dfc2ab785ebf31329
Chris Jones <chrisjones0517@gmail.com>
2018-03-07 23:13:12 -0600

    added extra functionality and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,72 @@
 $(document).ready(function () {
 
-    var searchObj ={
-        searchTerm:"",
-        numRecds : 0,
-        startYear:0,
-        endYear:0
-    }
+    $("#searchBtn").on("click", function () {
 
+        let search = $('#search').val();
+        let numRec = $('#numRec').val();
+        let startYear = $('#startYear').val();
+        let endYear = $('#endYear').val();
 
+        if (startYear > endYear) {
+            alert('Start year must be lower than end year!');
+        }
+        if (search === '') {
+            alert('You must enter a search term!');
+        }
+        if (startYear === '' || startYear < 1980) {
+            alert('You must enter a start year after 1980!');
+        }
+        if (endYear === '') {
+            alert('You must enter a end year!');
+        }
 
-    function getArticles() {
+        $('#info').empty();
+
         var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         url += '?' + $.param({
-            'api-key': 'a6fafa0f676b487eb61ae9b100d08673'
+            'api-key': "a6fafa0f676b487eb61ae9b100d08673",
+            'q': search,
+            'begin_date': startYear + '0101',
+            'end_date': endYear + '0101',
+            'page': numRec
         });
+
         $.ajax({
             url: url,
             method: 'GET',
         }).done(function (result) {
+
+            let articles = result.response.docs;
             console.log(result);
+            for (let i = 0; i < articles.length; i++) {
+                $('#info').append(`
+                <div class="article">
+                    <h3>${articles[i].headline.main}</h3>
+                    <p>${articles[i].snippet}</p>
+                    <a href="${articles[i].web_url}" target="_blank">full article</a>
+                </div>
+                <div class="spacer"></div>
+                `);
+            }
+            $('.article').css({
+                'border': '1px solid #428bca',
+                'border-radius': '3px',
+                'padding': '1rem'
+            });
+            $('.spacer').css('height','1rem');
+            
         }).fail(function (err) {
             throw err;
         });
-    }
-    $("#searchBtn").on("click",function(){
-      debugger
 
-      searchObj.searchTerm=$("#search").val();
-      //console.log(searchObj.searchTerm);
-      searchObj.numRecds=$("#exampleFormControlSelect1").val();    
-     // console.log(searchObj.numRecds);  
-      searchObj.startYear=$("#startYear").val(); 
-       //console.log(searchObj.startYear);
-      searchObj.endYear=$("#endYear").val(); 
-       //console.log(searchObj.endYear);
-     
+
+    });
+
+    $('#clearBtn').on('click', function() {
+        $('#info').empty();
+        $('#search').val('');
+        $('#numRec').val('');
+        $('#startYear').val('');
+        $('#endYear').val('');
     })
-    getArticles();
-})
+});
