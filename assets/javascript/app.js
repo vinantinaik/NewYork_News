b3a754d66f99bba4aa7dad5005967ca3fbbf3278
Chris Jones <chrisjones0517@gmail.com>
2018-03-08 09:43:31 -0600

    stricter input validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,37 +9,34 @@ $(document).ready(function () {
 
         if (startYear > endYear) {
             alert('Start year must be lower than end year!');
-        }
-        if (search === '') {
+        } else if (search === '') {
             alert('You must enter a search term!');
-        }
-        if (startYear === '' || startYear < 1980) {
-            alert('You must enter a start year after 1980!');
-        }
-        if (endYear === '') {
-            alert('You must enter a end year!');
-        }
+        } else if (startYear === '' || startYear < 1980 || startYear > 2018) {
+            alert('You must enter a start year between 1980 and the current year!');
+        } else if (endYear === '' || endYear > 2018) {
+            alert('You must enter a valid end year!');
+        } else {
 
-        $('#info').empty();
+            $('#info').empty();
 
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-            'api-key': "a6fafa0f676b487eb61ae9b100d08673",
-            'q': search,
-            'begin_date': startYear + '0101',
-            'end_date': endYear + '0101',
-            'page': numRec
-        });
+            var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+            url += '?' + $.param({
+                'api-key': "a6fafa0f676b487eb61ae9b100d08673",
+                'q': search,
+                'begin_date': startYear + '0101',
+                'end_date': endYear + '0101',
+                'page': numRec
+            });
 
-        $.ajax({
-            url: url,
-            method: 'GET',
-        }).done(function (result) {
+            $.ajax({
+                url: url,
+                method: 'GET',
+            }).done(function (result) {
 
-            let articles = result.response.docs;
-            console.log(result);
-            for (let i = 0; i < articles.length; i++) {
-                $('#info').append(`
+                let articles = result.response.docs;
+                console.log(result);
+                for (let i = 0; i < articles.length; i++) {
+                    $('#info').append(`
                 <div class="article">
                     <h3>${articles[i].headline.main}</h3>
                     <p>${articles[i].snippet}</p>
@@ -47,22 +44,21 @@ $(document).ready(function () {
                 </div>
                 <div class="spacer"></div>
                 `);
-            }
-            $('.article').css({
-                'border': '1px solid #428bca',
-                'border-radius': '3px',
-                'padding': '1rem'
+                }
+                $('.article').css({
+                    'border': '1px solid #428bca',
+                    'border-radius': '3px',
+                    'padding': '1rem'
+                });
+                $('.spacer').css('height', '1rem');
+
+            }).fail(function (err) {
+                throw err;
             });
-            $('.spacer').css('height','1rem');
-            
-        }).fail(function (err) {
-            throw err;
-        });
-
-
+        }
     });
 
-    $('#clearBtn').on('click', function() {
+    $('#clearBtn').on('click', function () {
         $('#info').empty();
         $('#search').val('');
         $('#numRec').val('');
